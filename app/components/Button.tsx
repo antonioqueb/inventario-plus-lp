23d2550b2c4968f445136c45a541ff657f7f596d
Antonio Queb <cardstillo@gmail.com>
2024-10-13 19:31:34 -0600

    Up

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -7,15 +7,15 @@ interface ButtonProps {
 }
 
 const Button: React.FC<ButtonProps> = ({ text, href, variant = 'default' }) => {
-  const baseClasses = "inline-block px-6 sm:px-8 py-3 sm:py-4 rounded-full text-base sm:text-lg font-bold transition-all duration-300 ease-in-out transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-offset-2 shadow-md";
-  
+  const baseClasses = "inline-flex justify-center items-center w-full sm:w-auto px-4 sm:px-6 py-3 sm:py-4 rounded-full text-sm sm:text-lg font-bold transition-all duration-300 ease-in-out transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-offset-2 shadow-md";
+
   const variantClasses = {
-    default: "bg-gradient-to-r from-sky-600 to-sky-700 text-white hover:from-sky-700 hover:to-sky-800 focus:ring-sky-500 my-8",
-    white: "bg-white text-sky-700 border-2 border-sky-700 hover:bg-sky-50 focus:ring-sky-700 my-8"
+    default: "bg-gradient-to-r from-sky-600 to-sky-700 text-white hover:from-sky-700 hover:to-sky-800 focus:ring-sky-500 my-4",
+    white: "bg-white text-sky-700 border-2 border-sky-700 hover:bg-sky-50 focus:ring-sky-700 my-4"
   };
 
   return (
-    <a 
+    <a
       href={href}
       className={`${baseClasses} ${variantClasses[variant]}`}
       role="button"
